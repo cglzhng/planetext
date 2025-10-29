b258525cfe94c0e89f7144564662043320cf6dcd
Cyril Zhang <cyril_zhang@sfu.ca>
2025-10-28 17:54:26 -0700

    Add dragging text around

--- src/canvas.js
+++ src/canvas.js
@@ -1,6 +1,104 @@
+import { generate_id } from './utils.js';
+
 const ZOOM_SPEED = 1.1;
 const MIN_ZOOM = 0.1;
 const MAX_ZOOM = 10;
+
+class TextBox {
+    #id;
+
+    #inner;
+    #box;
+    #x = 0;
+    #y = 0;
+
+    on_move;
+    on_focus;
+    on_blur;
+    on_change;
+    on_drag_start;
+
+    constructor(x, y) {
+        this.#id = generate_id();
+
+        this.#inner = document.createElement('div');
+        this.#inner.classList.add('text');
+        this.#inner.contentEditable = 'plaintext-only';
+
+        this.#box = document.createElement('div')
+        //this.#box.classList.add('textbox');
+        this.#box.style.position = 'absolute';
+
+        this.#box.appendChild(this.#inner);
+
+        this.move(x, y);
+
+        this.#box.focus();
+
+        this.#inner.addEventListener('blur', this.set_text.bind(this));
+        this.#inner.addEventListener('focus', this.edit_text.bind(this));
+        this.#inner.addEventListener('input', this.input_text.bind(this));
+
+        this.#box.addEventListener('mousedown', this.start_drag.bind(this));
+        this.#inner.addEventListener('mousedown', (e) => { e.stopPropagation(); });
+    }
+
+    get_id() {
+        return this.#id;
+    }
+
+    get_box() {
+        return this.#box;
+    }
+
+    get_text() {
+        return this.#inner.innerText;
+    }
+
+    get_position() {
+        return [this.#x, this.#y];
+    }
+
+    focus() {
+        this.#inner.focus();
+        this.on_focus?.();
+    }
+
+    move(x, y) {
+        this.#x = x;
+        this.#y = y;
+        this.#box.style.transform = `translate(${x}px, ${y}px)`;
+        this.on_move?.(x, y);
+    }
+
+    move_by(dx, dy) {
+        this.move(this.#x + dx, this.#y + dy);
+    }
+
+    edit_text() {
+        this.#box.classList.add('textbox');
+        this.on_focus?.();
+    }
+
+    input_text() {
+        this.on_change?.(this.get_text());
+    }
+
+    set_text() {
+        this.#box.classList.remove('textbox');
+        this.on_blur?.();
+    }
+
+    start_drag(e) {
+        if (e.button !== 0) {
+            return;
+        }
+        e.stopPropagation();
+        this.on_drag_start?.(e);
+    }
+
+
+}
 
 
 export class Canvas {
@@ -17,6 +115,8 @@ export class Canvas {
     #viewport_x = 0;
     #viewport_y = 0;
     #scale = 1;
+
+    #is_editing = false;
 
     constructor(content, width, height) {
         this.#content = content;
@@ -35,7 +135,7 @@ export class Canvas {
         this.#base.style.width = `${width}px`;
         this.#base.style.height = `${height}px`;
 
-        this.#base.addEventListener('click', this.create_text.bind(this));
+        this.#base.addEventListener('click', this.create_textbox.bind(this));
         this.#base.addEventListener('wheel', this.zoom.bind(this));
         this.#base.addEventListener('mousedown', this.start_pan.bind(this));
     }
@@ -70,6 +170,11 @@ export class Canvas {
         const x = -this.#viewport_x * this.#scale;
         const y = -this.#viewport_y * this.#scale;
         this.#base.style.transform = `translate(${x}px, ${y}px) scale(${this.#scale})`;
+    }
+
+    viewport_to_world(x, y) {
+        const rect = this.#base.getBoundingClientRect();
+        return [(x - rect.left) / this.#scale, (y - rect.top) / this.#scale];
     }
 
     zoom(e) {
@@ -125,65 +230,69 @@ export class Canvas {
         this.#base.addEventListener('mouseup', stop);
     }
 
-    create_text(e) {
+
+    create_textbox(e) {
         if (e.target !== this.#base) {
             return;
         }
-        const input = document.createElement('span');
-        input.classList.add('textbox');
-        input.classList.add('text');
-        input.contentEditable = 'true';
-        input.style.position = 'absolute';
-        const rect = base.getBoundingClientRect();
-        const x = (e.clientX - rect.left) / this.#scale;
-        const y = (e.clientY - rect.top) / this.#scale;
-        input.style.transform = `translate(${x}px, ${y}px)`;
-
-        this.#base.appendChild(input);
-        input.focus();
-
-        input.addEventListener('blur', () => this.set_text(input, x, y));
-        input.addEventListener('click', this.edit_text);
-    }
-
-    edit_text(e) {
-        const input = e.target;
-        input.classList.add('textbox');
-    }
-
-    set_text(input, x, y) {
-        if (input.innerText === '') {
-            input.remove();
+        if (this.#is_editing) {
+            this.#is_editing = false;
             return;
         }
-        const text = this.#content.add_text(input.innerText, x, y);
-        const id = text.id;
-        input.classList.remove('textbox');
-        this.#text_objects[id] = input;
+        const [x, y] = this.viewport_to_world(e.clientX, e.clientY);
 
-        const start_drag_text = (e) => {
-            if (e.button !== 0) {
-                return;
+        const textbox = new TextBox(x, y);
+        const box = textbox.get_box();
+
+        const id = textbox.get_id();
+        this.#text_objects[id] = textbox;
+        this.#content.add_text(id, x, y, "");
+
+        textbox.on_move = (new_x, new_y) => {
+            this.#content.move_text(id, new_x, new_y);
+        };
+
+        textbox.on_focus = () => {
+            this.#is_editing = true;
+        }
+
+        textbox.on_blur = () => {
+            if (box.innerText === '') {
+                box.remove();
+                delete this.#text_objects[id];
+                this.#content.remove_text(id);
+                this.#is_editing = false;
             }
-            const text = e.target;
-            const start_x = e.clientX;
-            const start_y = e.clientY;
-            const text_rect = text.getBoundingClientRect();
-            const base_rect = base.getBoundingClientRect();
-            const text_start_x = (text_rect.left - base_rect.left) / this.#scale;
-            const text_start_y = (text_rect.top - base_rect.top) / this.#scale;
+            else {
+                this.#content.set_text(id, textbox.get_text());
+            }
+        };
+
+        textbox.on_change = () => {
+            this.#content.set_text(id, textbox.get_text());
+        }
+
+        textbox.on_drag_start = (e) => {
+            let last_x = e.clientX;
+            let last_y = e.clientY;
             document.body.style.cursor = 'grabbing';
 
+            this.#is_editing = true;
+
             const drag = (e) => {
-                const dx = (e.clientX - start_x) / this.#scale;
-                const dy = (e.clientY - start_y) / this.#scale;
-                this.move_text(id, text_start_x + dx, text_start_y + dy);
+                const dx = (e.clientX - last_x) / this.#scale;
+                const dy = (e.clientY - last_y) / this.#scale;
+                textbox.move_by(dx, dy);
+                last_x = e.clientX;
+                last_y = e.clientY;
             }
 
-            const stop = () => {
+            const stop = (e) => {
                 base.removeEventListener('mousemove', drag);
                 base.removeEventListener('mouseleave', stop);
                 base.removeEventListener('mouseup', stop);
+                textbox.focus();
+                e.stopPropagation();
                 document.body.style.cursor = 'default';
             }
 
@@ -192,16 +301,10 @@ export class Canvas {
             base.addEventListener('mouseup', stop);
         }
 
-        input.addEventListener('mousedown', start_drag_text);
+        this.#base.appendChild(box);
+        textbox.focus();
 
     }
-
-    move_text(id, x, y) {
-        this.#content.move_text(id, x, y);
-        const text_object = this.#text_objects[id];
-        text_object.style.transform = `translate(${x}px, ${y}px)`;
-    }
-
 
 
 }
--- src/content.js
+++ src/content.js
@@ -2,30 +2,38 @@ import { generate_id } from './utils.js';
 
 
 export class Content {
+    #text_by_id = {}
     constructor() {
-        this.text_by_id = {};
     }
 
     move_text(id, x, y) {
-        const text = this.text_by_id[id];
+        const text = this.#text_by_id[id];
         text.x = x;
         text.y = y;
     }
 
     get_text(id) {
-        return this.text_by_id[id];
+        return this.#text_by_id[id];
     }
 
-    add_text(s, x, y) {
-        const id = generate_id();
+    set_text(id, s) {
+        const text = this.#text_by_id[id];
+        text.content = s;
+        console.log(this.#text_by_id)
+    }
+
+    remove_text(id) {
+        delete this.#text_by_id[id];
+    }
+
+    add_text(id, x, y, s="") {
         const text = {
             id: id,
             content: s,
             x: x,
             y: y,
         };
-        this.text_by_id[id] = text;
-        return text;
+        this.#text_by_id[id] = text;
     }
 
 }
